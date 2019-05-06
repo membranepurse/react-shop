3a78d71c1a1e5212b9d747b3f398a369ff834e7a
Sager <johndoe@example.com>
2019-05-05 23:24:38 -0500

    Updates

---
+++ precache-manifest.f88db8c11d8238c6d4fb41c121902f17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/reactShop/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "2e648eff9622c497116c",
+    "url": "/reactShop/static/js/runtime~main.2e648eff.js"
+  },
+  {
+    "revision": "4a0bede8980d15cff341",
+    "url": "/reactShop/static/js/main.4a0bede8.chunk.js"
+  },
+  {
+    "revision": "7fdb9914bc892bc87951",
+    "url": "/reactShop/static/js/1.7fdb9914.chunk.js"
+  },
+  {
+    "revision": "4a0bede8980d15cff341",
+    "url": "/reactShop/static/css/main.a99bebf0.chunk.css"
+  },
+  {
+    "revision": "7fdb9914bc892bc87951",
+    "url": "/reactShop/static/css/1.bcff7d7b.chunk.css"
+  },
+  {
+    "revision": "b37078c1b139c2d9659b7f0b323660a0",
+    "url": "/reactShop/index.html"
+  }
+];
